a10c1cc163b4d67b243a6cd096c30cbcd8e96952
felipe mascena <felipe.mascena.dev@gmail.com>
2024-11-02 14:18:16 -0300

    alterando footer

--- desafio-05/calculadora/src/Components/Footer/Footer.jsx
+++ desafio-05/calculadora/src/Components/Footer/Footer.jsx
@@ -1,11 +1,13 @@
-import './Footer.scss'
+import './Footer.scss';
 
 export default function Footer() {
   return (
     <footer>
       <h3>
         Desafio 05 desenvolvido por{' '}
-        <a href="https://linkedin.com/in/felipe-mascena">Felipe Mascena</a>{' '}
+        <a href="https://linkedin.com/in/felipe-mascena" target="_blank">
+          Felipe Mascena
+        </a>{' '}
       </h3>
     </footer>
   );
