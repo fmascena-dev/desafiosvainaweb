e8156c1fcea3fb2de93db70604b92db053ec38e1
felipe mascena <felipe.mascena.dev@gmail.com>
2024-11-11 23:48:41 -0300

    desafio-final

---
+++ desafio-final/src/Components/Sobre/Sobre.jsx
@@ -0,0 +1,9 @@
+export default function Sobre() {
+    return (
+        <section className="sobre">
+            <h2>Sobre</h2>
+            <p>Este é um simples site feito com React, Vite e SASS.</p>
+            <p>Você pode ver o código fonte <a href="https://github.com/felipesantana/desafios-vainaweb">aqui</a>.</p>
+        </section>
+    )
+};
